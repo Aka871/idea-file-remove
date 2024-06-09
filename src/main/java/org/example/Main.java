719e97580ab1f39b44d38b59c159fa3ee62aa8e6
Aka871 <163530203+Aka871@users.noreply.github.com>
2024-06-09 17:24:38 +0900

    名前を変更

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ import java.util.Map;
 public class Main {
 
   public static void main(String[] args) {
-    List<String> studentNameList = List.of("加藤", "鈴木", "七五三掛", "田中", "田母神",
+    List<String> studentNameList = List.of("加藤", "香山", "七五三掛", "田中", "田母神",
         "小鳥遊", "山口", "伊藤", "田口", "山田");
   }
 }
